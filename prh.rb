282ece6a7b59f41193232aa2e05b55aece3b8807
Kayvan <kayvannj@users.noreply.github.com>
2017-01-17 12:44:53 -0800

    Update prh.rb

--- prh.rb
+++ prh.rb
@@ -4,8 +4,8 @@
 class Prh < Formula
   desc "A tool to help creating PullRequests on Github easier and automate some of the steps"
   homepage "https://github.com/kayvannj/PullRequestHelper"
-  url "https://github.com/kayvannj/PullRequestHelper/archive/2.0.0.tar.gz"
-  sha256 "d67b087c7bf65f8c7018374280db22a036db759099614ce1f4eb7b7ae0af9693"
+  url "https://github.com/kayvannj/PullRequestHelper/archive/2.0.1.tar.gz"
+  sha256 "c74390d56ac9deac3e8da6fff5cc30570a34300b6e2df0dc1bf08d6dfa211b29"
 
   resource "requests" do
     url "https://pypi.python.org/packages/5b/0b/34be574b1ec997247796e5d516f3a6b6509c4e064f2885a96ed885ce7579/requests-2.12.4.tar.gz#md5=acdb48888a9d3c7309da12fc7f83fedb"
